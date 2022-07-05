e7574f009143cd054ef534eda7d3606f15657c36
Kristin Kleyla <clever_disguises@yahoo.com>
2022-07-04 20:40:09 -0400

    started building routes for updating posts on dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,59 +1,81 @@
 const router = require('express').Router();
-const {Blog, User} = require('../models');
+const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-      const blogData = await Blog.findAll({
-          attributes: ['id', 'title', 'text', 'created_at'],
-          where: {
-              user_id: req.session.userId,
-          },
-          include: [
-              {
-                  model: User,
-                  attributes: ['username'],
-              },
-          ],
-      });
+    const blogData = await Blog.findAll({
+      attributes: ['id', 'title', 'text', 'created_at'],
+      where: {
+        user_id: req.session.userId,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-      const blogs = blogData.map((blog) => blog.get({plain: true}));
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-      res.render('dashboard', {
-          blogs,
-          loggedIn: req.session.loggedIn,
-      });
+    res.render('dashboard', {
+      blogs,
+      loggedIn: req.session.loggedIn,
+    });
 
   } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
 router.get('/new', withAuth, async (req, res) => {
   try {
-      res.render('new-post', {
-          loggedIn: req.session.loggedIn,
-      });
+    res.render('new-post', {
+      loggedIn: req.session.loggedIn,
+    });
 
   } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
 router.post('/new', withAuth, async (req, res) => {
   try {
-      const blogData = await Blog.create({
-          title: req.body.title,
-          text: req.body.text,
-          user_id: req.session.userId,
-      });
+    const blogData = await Blog.create({
+      title: req.body.title,
+      text: req.body.text,
+      user_id: req.session.userId,
+    });
 
-      res.status(200).json(blogData);
+    res.status(200).json(blogData);
   } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      attributes: ['title', 'text', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render('update-blog', { blog, loggedIn: req.session.loggedIn });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
